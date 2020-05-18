ee4973f68025d09957bbb9771738954926cc3af6
Jorge Perez <jorge.perez@wizeline.com>
2020-05-18 10:21:58 -0500

    fix babel

--- wdio-omdb-automation/babel.config.js
+++ wdio-omdb-automation/babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     presets: [
-        [ '@babel/preset-env', {
+        ['@babel/preset-env', {
             targets: {
                 node: 8
             },
-            configPath: './node_modules'
+            configPath = __dirname
         } ]
     ]
 };
